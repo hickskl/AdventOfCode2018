803d5fc7efa15c9bb5fcaa4a9e8c11ea8fc15cfd
Kendra <hicksk@mail.com>
2018-12-07 22:40:27 -0500

    Initial Commit

---
+++ Day5/Day5_1.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+def setup():
+    global fileHandle, fileData
+
+    filename = input("Enter an input file name: ")
+    exists = os.path.isfile("./%s" % filename)
+    notEmpty = os.path.getsize("./%s" % filename) > 0
+
+    if exists and notEmpty:
+        fileHandle = open ("./%s" % filename, "r")
+    else:
+        print ("File doesn't exist or is empty.")
+        exit
+     
+    fileData = list()
+    for entry in fileHandle:
+        fileData.append(entry)
+    fileHandle.close()
+
+setup()
